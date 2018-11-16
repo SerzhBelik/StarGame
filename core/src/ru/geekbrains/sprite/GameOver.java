1f4a24ae3eb644349fb604e34a70415ae4a962cd
SerzhBelik <sergeybelik86@gmail.com>
2018-11-16 20:59:04 +0400

    система бонусов: оружие, аптечки и щит; пауза

--- core/src/ru/geekbrains/sprite/GameOver.java
+++ core/src/ru/geekbrains/sprite/GameOver.java
@@ -9,7 +9,7 @@ import ru.geekbrains.math.Rect;
 public class GameOver extends Sprite {
     public GameOver(TextureAtlas atlas) {
         super(atlas.findRegion("message_game_over"));
-        setHeightProportion(0.08f);
+        setHeightProportion(0.075f);
         pos.set(0, 0.1f);
     }
 }
